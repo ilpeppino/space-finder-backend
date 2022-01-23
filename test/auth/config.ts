0b8729fe7d52a1b5951fe82a40f79675fd5ccb78
gtempone <giuseppe.tempone@nnip.com>
2022-01-23 16:03:07 +0100

    auth

---
+++ test/auth/config.ts
@@ -0,0 +1,9 @@
+
+
+export const config = {
+    REGION          : "us-east-1",
+    USER_POOL_ID    : "us-east-1_6NrX8FTA2",
+    APP_CLIENT_ID   : "6h43sp5ookin428n3t3pl5sflm",
+    USERNAME        : "ilpeppino",
+    PASSWORD        : "Efisdims5!a"
+}
